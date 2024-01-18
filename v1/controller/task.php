4d7a4170475916e8f9c6d5c6052bc14646699ace
Brou Kouadio Stéphane Fabien <fabienbrou99@gmail.com>
2024-01-18 12:03:25 +0000

    endpoint pour l'obtention de toutes les taches en cour

--- v1/controller/task.php
+++ v1/controller/task.php
@@ -11,7 +11,39 @@
         $response = new Response();
         $response->setHttpStatusCode(500);
         $response->setSuccess(false);
-        $response->addMessage("Database connection error");
+        $response->addMessage("Database connection error!");
         $response->send();
         exit;
+    }
+
+    // Vérifier s'il y a un paramètre 'taskid'
+    if(array_key_exists('taskid', $_GET)){
+        $taskid = $_GET['taskid'];
+        // Vérifier que la valeur de taskid est correcte
+        if($taskid == '' || $taskid<=0 || !is_numeric($taskid)){
+            $response = new Response();
+            $response->setHttpStatusCode(400);
+            $response->setSuccess(false);
+            $response->addMessage("Invalid taskid! Must be numeric and greater than 0");
+            $response->send();
+            exit;
+        }
+
+        // Agir en fonction de la méthode utilisée
+        if($_SERVER['REQUEST_METHOD'] === 'GET'){
+
+        }
+        elseif($_SERVER['REQUEST_METHOD'] === 'PUT' || $_SERVER['REQUEST_METHOD'] === 'PATCH'){
+
+        }
+        elseif($_SERVER['REQUEST_METHOD'] === 'DELETE'){
+            
+        }else{
+            $response = new Response();
+            $response->setHttpStatusCode(405);
+            $response->setSuccess(false);
+            $response->addMessage("Method Not Allowed!");
+            $response->send();
+            exit;
+        }
     }
