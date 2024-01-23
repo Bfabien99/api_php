937c0173bd1b15242946eba058537046d8ce9906
Brou Kouadio Stéphane Fabien <fabienbrou99@gmail.com>
2024-01-23 10:43:47 +0000

    delete a task and get completed/incompleted tasks

--- v1/controller/task.php
+++ v1/controller/task.php
@@ -82,7 +82,37 @@
             }
         }
         elseif($_SERVER['REQUEST_METHOD'] === 'DELETE'){
-            
+            try{
+                $query = $readDB->prepare('DELETE FROM tbltasks WHERE id = :taskid');
+                $query->bindParam(':taskid', $taskid, PDO::PARAM_INT);
+                $query->execute();
+
+                $rowCount = $query->rowCount();
+
+                if($rowCount == 0){
+                    $response = new Response();
+                    $response->setHttpStatusCode(404);
+                    $response->setSuccess(false);
+                    $response->addMessage("Task not found!");
+                    $response->send();
+                    exit;
+                }
+                $response = new Response();
+                $response->setHttpStatusCode(200);
+                $response->setSuccess(true);
+                $response->addMessage("Task deleted!");
+                $response->send();
+                exit;
+            }
+            catch(PDOException $pDOException){
+                error_log("Connection error - ". $pDOException->getMessage(), 0);
+                $response = new Response();
+                $response->setHttpStatusCode(500);
+                $response->setSuccess(false);
+                $response->addMessage("Database connection error!");
+                $response->send();
+                exit;
+            }
         }
         elseif($_SERVER['REQUEST_METHOD'] === 'PUT' || $_SERVER['REQUEST_METHOD'] === 'PATCH'){
 
@@ -92,6 +122,70 @@
             $response->setHttpStatusCode(405);
             $response->setSuccess(false);
             $response->addMessage("Method Not Allowed!");
+            $response->send();
+            exit;
+        }
+    }
+    // Récupérer toutes les tâches complète
+    if(array_key_exists('completed', $_GET)){
+        $completed = $_GET['completed'];
+        // Vérifier que la valeur de completed est correcte
+        if($completed !== 'Y' && $completed !== 'N'){
+            $response = new Response();
+            $response->setHttpStatusCode(400);
+            $response->setSuccess(false);
+            $response->addMessage("Completed must be Y or N");
+            $response->send();
+            exit;
+        }
+
+        if($_SERVER['REQUEST_METHOD'] === 'GET'){
+            try{
+                $query = $readDB->prepare('SELECT id, title, description, DATE_FORMAT(deadline, "%d/%m/%Y %H:%i") AS deadline, completed FROM tbltasks WHERE completed = :completed');
+                $query->bindParam(':completed', $completed, PDO::PARAM_STR);
+                $query->execute();
+
+                $rowCount = $query->rowCount();
+
+                $taskArray=[];
+                while($row = $query->fetch()){
+                    $task = new Task($row['id'], $row['title'], $row['description'],$row['deadline'],$row['completed']);
+                    $taskArray[] = $task->returnTaskAsArray();
+                }
+                $returnData = [];
+                $returnData['rows_returned'] = $rowCount;
+                $returnData['tasks'] = $taskArray;
+
+                $response = new Response();
+                $response->setHttpStatusCode(200);
+                $response->setSuccess(true);
+                $response->toCache(true);
+                $response->setData($returnData);
+                $response->send();
+                exit;
+
+            }catch(PDOException $pDOException){
+                error_log("Connection error - ". $pDOException->getMessage(), 0);
+                $response = new Response();
+                $response->setHttpStatusCode(500);
+                $response->setSuccess(false);
+                $response->addMessage("Database connection error!");
+                $response->send();
+                exit;
+            }catch(TaskException $taskException){
+                error_log("Database query error - ". $taskException->getMessage(), 0);
+                $response = new Response();
+                $response->setHttpStatusCode(500);
+                $response->setSuccess(false);
+                $response->addMessage("Failed to get Task!");
+                $response->send();
+                exit;
+            }
+        }else{
+            $response = new Response();
+            $response->setHttpStatusCode(405);
+            $response->setSuccess(false);
+            $response->addMessage("Method not allowed!");
             $response->send();
             exit;
         }
