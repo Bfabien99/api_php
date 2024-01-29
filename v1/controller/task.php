da2e402819b02e8ac47aeab7df2a8957a5e68a6a
Brou Kouadio Stéphane Fabien <fabienbrou99@gmail.com>
2024-01-29 11:20:58 +0000

    get tasks with pagination

--- v1/controller/task.php
+++ v1/controller/task.php
@@ -190,6 +190,112 @@
             exit;
         }
     }
+    // Récupérer les tâches avec pagination
+    elseif(array_key_exists('page', $_GET)){
+        $page = $_GET['page'];
+        // Vérifier que la valeur de page est correcte
+        if($page == '' || $page<=0 || !is_numeric($page)){
+            $response = new Response();
+            $response->setHttpStatusCode(400);
+            $response->setSuccess(false);
+            $response->addMessage("Invalid page! Must be numeric and greater than 0");
+            $response->send();
+            exit;
+        }
+
+        // Agir en fonction de la méthode utilisée
+        if($_SERVER['REQUEST_METHOD'] === 'GET'){
+            $limitPerPage = 20;
+            try{
+                // Récupérer le nombre total de tâche
+                $query = $readDB->prepare('SELECT count(id) AS totalNoOfTasks from tbltasks');
+                $query->execute();
+
+                $row = $query->fetch();
+                $tasksCount = intval($row['totalNoOfTasks']);
+
+                // Déterminer le nombre total de page
+                $numOfPages = ceil($tasksCount/$limitPerPage);
+
+                if($numOfPages == 0){
+                    $numOfPages = 1;
+                }
+
+                if($page > $numOfPages){
+                    $response = new Response();
+                    $response->setHttpStatusCode(404);
+                    $response->setSuccess(false);
+                    $response->addMessage("Page not found!");
+                    $response->send();
+                    exit;
+                }
+
+                $offset = ($page == 1 ? 0 : ($limitPerPage*($page-1)));
+
+                $query = $readDB->prepare('SELECT id, title, description, DATE_FORMAT(deadline, "%d/%m/%Y %H:%i") AS deadline, completed from tbltasks limit :pglimit offset :offset');
+                $query->bindParam(':pglimit', $limitPerPage, PDO::PARAM_INT);
+                $query->bindParam(':offset', $offset, PDO::PARAM_INT);
+                $query->execute();
+
+                $rowCount = $query->rowCount();
+
+                if($rowCount == 0){
+                    $response = new Response();
+                    $response->setHttpStatusCode(404);
+                    $response->setSuccess(false);
+                    $response->addMessage("Task not found!");
+                    $response->send();
+                    exit;
+                }
+
+                while($row = $query->fetch()){
+                    $task = new Task($row['id'], $row['title'], $row['description'],$row['deadline'],$row['completed']);
+                    $taskArray[] = $task->returnTaskAsArray();
+                }
+                $returnData = [];
+                $returnData['rows_returned'] = $rowCount;
+                $returnData['total_rows'] = $tasksCount;
+                $returnData['total_page'] = $numOfPages;
+                $returnData['current_page'] = intval($page);
+                $returnData['has_next_page'] = ($page<$numOfPages);
+                $returnData['has_previous_page'] = ($page>1);
+                $returnData['tasks'] = $taskArray;
+
+                $response = new Response();
+                $response->setHttpStatusCode(200);
+                $response->setSuccess(true);
+                $response->toCache(true);
+                $response->setData($returnData);
+                $response->send();
+                exit;
+
+            }catch(PDOException $pDOException){
+                error_log("Connection error - ". $pDOException->getMessage(), 0);
+                $response = new Response();
+                $response->setHttpStatusCode(500);
+                $response->setSuccess(false);
+                $response->addMessage("Database connection error!");
+                $response->send();
+                exit;
+            }catch(TaskException $taskException){
+                error_log("Database query error - ". $taskException->getMessage(), 0);
+                $response = new Response();
+                $response->setHttpStatusCode(500);
+                $response->setSuccess(false);
+                $response->addMessage("Failed to get Task!");
+                $response->send();
+                exit;
+            }
+        }
+        else{
+            $response = new Response();
+            $response->setHttpStatusCode(405);
+            $response->setSuccess(false);
+            $response->addMessage("Method Not Allowed!");
+            $response->send();
+            exit;
+        }
+    }
     elseif(empty($_GET)){
         if($_SERVER['REQUEST_METHOD'] === 'GET'){
             try{
@@ -233,7 +339,7 @@
             }
         }
         elseif($_SERVER['REQUEST_METHOD'] === 'POST'){
-
+        
         }
         else{
             $response = new Response();
